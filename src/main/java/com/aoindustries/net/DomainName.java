3b66f2d05767e9422393e076a5e83bf3519e0c4a
AO Industries, Inc. <support@aoindustries.com>
2017-08-22 00:54:46 -0500

    New constants for localhost and localhost.localdomain.

--- src/main/java/com/aoindustries/net/DomainName.java
+++ src/main/java/com/aoindustries/net/DomainName.java
@@ -72,6 +72,21 @@ implements
 
 	public static final int MAX_LENGTH = 253;
 
+	public static final DomainName
+		LOCALHOST,
+		LOCALHOST_LOCALDOMAIN
+	;
+	static {
+		try {
+			LOCALHOST = new DomainName("localhost").intern();
+			LOCALHOST_LOCALDOMAIN = new DomainName("localhost.localdomain").intern();
+		} catch(ValidationException e) {
+			AssertionError ae = new AssertionError("These hard-coded values are valid");
+			ae.initCause(e);
+			throw ae;
+		}
+	}
+
 	/*
 	private static boolean isNumeric(String label) {
 		return isNumeric(label, 0, label.length());
