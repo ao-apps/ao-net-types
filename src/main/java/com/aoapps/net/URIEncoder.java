c39ba18392527e9a5084cb86726d00f483391ca1
AO Industries, Inc <support@aoindustries.com>
2023-09-07 09:22:45 -0500

    Unicode right arrows "→" instead of "->"

--- src/main/java/com/aoapps/net/URIEncoder.java
+++ src/main/java/com/aoapps/net/URIEncoder.java
@@ -1,6 +1,6 @@
 /*
  * ao-net-types - Networking-related value types.
- * Copyright (C) 2019, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2019, 2020, 2021, 2022, 2023  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -254,7 +254,7 @@ public final class URIEncoder {
         }
       } else {
         if (replacement != null) {
-          throw new AssertionError("Character is not in rfc3986ReservedCharacters_percent_and_invalid but is encoded: " + ch + " -> " + replacement);
+          throw new AssertionError("Character is not in rfc3986ReservedCharacters_percent_and_invalid but is encoded: " + ch + " → " + replacement);
         }
       }
     }
