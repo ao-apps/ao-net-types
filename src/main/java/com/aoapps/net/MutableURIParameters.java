9cb070184a04a995b0960e0eb1530af6b361eb09
AO Industries, Inc <support@aoindustries.com>
2025-03-13 13:46:42 -0500

    Do not use wildcard generic bounds on final types

--- src/main/java/com/aoapps/net/MutableURIParameters.java
+++ src/main/java/com/aoapps/net/MutableURIParameters.java
@@ -1,6 +1,6 @@
 /*
  * ao-net-types - Networking-related value types.
- * Copyright (C) 2011, 2016, 2019, 2021, 2022, 2024  AO Industries, Inc.
+ * Copyright (C) 2011, 2016, 2019, 2021, 2022, 2024, 2025  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -146,7 +146,7 @@ public interface MutableURIParameters extends URIParameters {
    * @deprecated  Please use {@link #add(java.lang.String, java.lang.Iterable)} directly.
    */
   @Deprecated
-  default void addParameters(String name, Iterable<? extends String> values) {
+  default void addParameters(String name, Iterable<String> values) {
     if (values != null) {
       for (String value : values) {
         addParameter(name, value);
